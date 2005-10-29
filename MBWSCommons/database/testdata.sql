5bf5216050c9381bb7931d5fab2d9b2296423d1f
azarai <azarai@5ae0fa9c-bcfc-0310-8243-f99ef3d6a034>
2005-10-29 00:12:45 +0000

    added script for testdata

---
+++ MBWSCommons/database/testdata.sql
@@ -0,0 +1,130 @@
+# MySQL-Front Dump 2.2
+#
+# Host: localhost   Database: game
+#--------------------------------------------------------
+# Server version 4.1.12a
+
+
+#
+# Dumping data for table 'account'
+#
+INSERT INTO account VALUES("minisack","minisack","minisack@sack.de");
+INSERT INTO account VALUES("sack","sack","sack@sack.de");
+
+
+#
+# Dumping data for table 'character_item_mapping'
+#
+
+
+#
+# Dumping data for table 'character_skill_mapping'
+#
+
+
+#
+# Dumping data for table 'character_status'
+#
+INSERT INTO character_status VALUES("1","A","0","0","1","1","10","0","0","0","0","0","0","0","0","0");
+INSERT INTO character_status VALUES("2","A","0","0","1","10","1","0","0","0","0","0","0","0","0","0");
+
+
+#
+# Dumping data for table 'character_visualappearance'
+#
+INSERT INTO character_visualappearance VALUES("1","1","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0");
+INSERT INTO character_visualappearance VALUES("2","2","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0");
+
+
+#
+# Dumping data for table 'character_worldobject_mapping'
+#
+
+
+#
+# Dumping data for table 'characterdata'
+#
+INSERT INTO characterdata VALUES("1","Sir Sackerus","1","sack","0","0","0","0","0","0","0","0","0");
+INSERT INTO characterdata VALUES("2","Mr SugaMaDig","1","minisack","0","0","0","0","0","0","0","0","0");
+
+
+#
+# Dumping data for table 'configuration'
+#
+
+
+#
+# Dumping data for table 'item'
+#
+
+
+#
+# Dumping data for table 'item_type'
+#
+
+
+#
+# Dumping data for table 'map'
+#
+INSERT INTO map VALUES("1","Testkarte","");
+
+
+#
+# Dumping data for table 'npc'
+#
+
+
+#
+# Dumping data for table 'npc_skill_mapping'
+#
+
+
+#
+# Dumping data for table 'npc_status'
+#
+
+
+#
+# Dumping data for table 'npc_worldobject_mapping'
+#
+
+
+#
+# Dumping data for table 'race'
+#
+INSERT INTO race VALUES("1","Mensch","0","0","0","0","0","0","0","0","Ganz toll, zerstören alles was ihnen in den Weg kommt.");
+
+
+#
+# Dumping data for table 'skill'
+#
+
+
+#
+# Dumping data for table 'skill_group'
+#
+
+
+#
+# Dumping data for table 'skill_race_mapping'
+#
+
+
+#
+# Dumping data for table 'worldobject'
+#
+
+
+#
+# Dumping data for table 'worldobject_type'
+#
+
+
+#
+# Dumping data for table 'zoneserver'
+#
+
+
+#
+# Dumping data for table 'zoneserver_map_mapping'
+#
