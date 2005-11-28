a79279dba2c1f5394e346e426bfabcf8187d53e4
azarai <azarai@5ae0fa9c-bcfc-0310-8243-f99ef3d6a034>
2005-11-28 21:20:25 +0000

    Tools for the game system

---
+++ MBWSTools/src/de/mbws/tools/LanguageFileCreator.java
@@ -0,0 +1,78 @@
+package de.mbws.tools;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import de.mbws.common.data.generated.Language;
+import de.mbws.common.data.generated.LanguageTextMapping;
+import de.mbws.server.persistence.BasePersistenceManager;
+
+/**
+ * Description: 
+ * @author Azarai
+ *
+ */
+public class LanguageFileCreator  {
+    private static Logger logger = Logger.getLogger(LanguageFileCreator.class);
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws Exception{
+        BasePersistenceManager.init();
+        org.hibernate.Session session = null;
+        try {
+            BasicConfigurator.configure();
+            PropertyConfigurator.configure("log4j.properties");
+            logger.info("Init log4j ... done");
+            Configuration cfg = new Configuration().configure(new File("../MBWSCommons/mapping/hibernate.cfg.xml"));
+            SessionFactory sessions = cfg.buildSessionFactory();
+            session = sessions.openSession();
+            List languages = session.createQuery("from Language").list();
+            for (Iterator iter = languages.iterator(); iter.hasNext();) {
+                Language element = (Language) iter.next();
+                Set map = null;
+                map = element.getLanguageTextMappings();
+                Properties props = new Properties();
+                
+                for (Iterator iterator = map.iterator(); iterator.hasNext();) {
+                    LanguageTextMapping ltm = (LanguageTextMapping) iterator.next();
+                    System.out.println(element.getShortname() + "  " + ltm.getText());
+                    props.put(ltm.getTextKey(), ltm.getText());
+                }
+                File out = new File("LanguageBundle_" + element.getShortname() + ".properties");
+                FileOutputStream fout = new FileOutputStream(out);
+                props.store(fout,"");
+            }
+
+            
+//            ResourceBundle myResources =
+//                PropertyResourceBundle.getBundle("Bundle", Locale.getDefault());
+//            Enumeration num = myResources.getKeys();
+//            while (num.hasMoreElements()) {
+//                String element = (String) num.nextElement();
+//                System.out.println(element);
+//            }
+        } catch (Exception e) {
+            logger.error("Error during LanguageMapping retrieving", e);
+        } finally {
+            try {
+                if (session != null) {
+                    session.close();
+                }
+            } catch (Exception e) {
+                logger.error("Error during session closing", e);
+            }
+        }
+    }
+
+}
