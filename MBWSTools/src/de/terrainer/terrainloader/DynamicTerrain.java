57e2b0ddaeca547477230c88def99d2ae3039f3b
crypter <crypter@5ae0fa9c-bcfc-0310-8243-f99ef3d6a034>
2006-01-24 12:24:21 +0000

    Fixed some bugs in adding of terrains. Added intelligent removal of terrains.

--- MBWSTools/src/de/terrainer/terrainloader/DynamicTerrain.java
+++ MBWSTools/src/de/terrainer/terrainloader/DynamicTerrain.java
@@ -2,9 +2,8 @@ package de.terrainer.terrainloader;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
-
-import javax.xml.parsers.ParserConfigurationException;
 
 import org.xml.sax.SAXException;
 
@@ -15,6 +14,12 @@ import com.jme.system.DisplaySystem;
 import com.jmex.terrain.TerrainBlock;
 
 /**
+ * DynamicTerrain is a super node for the terrain of a world. The world is
+ * described in a xml-file and consists of various quadratic terrains forming a
+ * rectangular world. The quadratic terrains reside in the same directory as the
+ * world description. They are loaded dynamically and attached as subnodes, when
+ * they are within a predefined visibility radius of the camera.
+ * 
  * @author Axel Sammet
  */
 public class DynamicTerrain extends Node {
@@ -25,16 +30,20 @@ public class DynamicTerrain extends Node {
 	float spatialScale = 5;
 	float heightScale = 0.3f;
 	int sectionResolution = 129;
-	float sectionWidth = spatialScale * (sectionResolution-1);
-	float visibilityRadius2 = 9*sectionWidth*sectionWidth;
-	float prefetchRadius = 3*sectionWidth;
+	float sectionWidth = spatialScale * (sectionResolution - 1);
+	float visibilityRadius = 3 * sectionWidth;
+	float visibilityRadius2 = visibilityRadius * visibilityRadius;
+	float prefetchRadius = 3 * sectionWidth;
+	float prefetchRadius2 = prefetchRadius * prefetchRadius;
 	float unloadRadius;
 
+	// TODO Check if Sets are sufficient
 	Map<String, TerrainBlock> sectionCache = new HashMap<String, TerrainBlock>();
+	Map<String, TerrainBlock> visibleSections = new HashMap<String, TerrainBlock>();
 
 	TerrainLoader terrainLoader;
 	DisplaySystem display;
-	
+
 	public DynamicTerrain() {
 		super("DynamicTerrain");
 	}
@@ -43,7 +52,6 @@ public class DynamicTerrain extends Node {
 	 * @param pathForWorldDescription
 	 *            path to the world description file <b> without </b> file
 	 *            extension.
-	 * @throws ParserConfigurationException
 	 * @throws IOException
 	 * @throws SAXException
 	 */
@@ -54,49 +62,60 @@ public class DynamicTerrain extends Node {
 		this.terrainLoader = new TerrainLoader(this);
 		terrainLoader.loadWorldDescription(worldPath + ".wld");
 
+		// TODO replace by dynamic prefetching
 		for (int x = 0; x < sectionColumns; x++) {
 			for (int y = 0; y < sectionRows; y++) {
 				sectionCache.put(x + "_" + y, terrainLoader.loadTerrainBlock(x, y));
 			}
 		}
-		
 
 	}
 
 	private void addVisibleSections(Vector3f position) {
 		int xstart = Math.max((int) ((position.x - prefetchRadius) / sectionWidth), 0);
-		int xend = Math.min((int) ((position.x + prefetchRadius) / sectionWidth), sectionColumns-1);
+		int xend = Math.min((int) ((position.x + prefetchRadius) / sectionWidth),
+				sectionColumns - 1);
 		int ystart = Math.max((int) ((position.y - prefetchRadius) / sectionWidth), 0);
-		int yend = Math.min((int) ((position.y + prefetchRadius) / sectionWidth), sectionRows-1);
+		int yend = Math.min((int) ((position.y + prefetchRadius) / sectionWidth), sectionRows - 1);
 		for (int x = xstart; x < xend; x++) {
 			for (int y = ystart; y < yend; y++) {
 				Vector3f sectionPosition = new Vector3f(x * sectionWidth + sectionWidth / 2, 0, y
 						* sectionWidth + sectionWidth / 2);
-				if (isInVisibleRange(position, sectionPosition)) {
-					TerrainBlock tb = sectionCache.get(x + "_" + y);
+				String key = x + "_" + y;
+				if (isInVisibleRange(position, sectionPosition) && !visibleSections.containsKey(key)) {
+					TerrainBlock tb = sectionCache.get(key);
 					attachChild(tb);
+					visibleSections.put(key, tb);
 				}
 			}
 		}
 	}
 
 	private void removeInvisibleSections(Vector3f position) {
-
+		Iterator<TerrainBlock> it = visibleSections.values().iterator();
+		while (it.hasNext()) {
+			TerrainBlock tb = it.next();
+			Vector3f midPoint = new Vector3f(tb.getLocalTranslation().x + sectionWidth / 2, 0, tb
+					.getLocalTranslation().z
+					+ sectionWidth / 2);
+			if (!isInVisibleRange(midPoint, position)) {
+				tb.removeFromParent();
+			}
+		}
 	}
 
 	private boolean isInVisibleRange(Vector3f pos1, Vector3f pos2) {
-		return (pos1.x * pos1.x - pos2.x * pos2.x + pos1.y * pos1.y - pos2.y * pos2.y) < visibilityRadius2;
+		return (pos1.x * pos1.x - pos2.x * pos2.x + pos1.z * pos1.z - pos2.z * pos2.z) < visibilityRadius2;
 	}
 
 	public void update(Camera cam) {
 		Vector3f location = cam.getLocation();
 		Vector3f viewdir = cam.getDirection();
-		
+
 		// identify visible sections
-		
+
 		// remove sections from the model, that are not visible anymore
-		if (getChildren() != null)
-			detachAllChildren();
+		removeInvisibleSections(location);
 
 		// add visible sections, that are still missing in the model
 		addVisibleSections(location);
