0ca8bac409a2cef83b9cbd5630d3aecec7da7c0d
kerim <kerim@5ae0fa9c-bcfc-0310-8243-f99ef3d6a034>
2006-01-30 07:25:15 +0000

    changed quotation marks into '

--- MBWSCommons/database/testdata.sql
+++ MBWSCommons/database/testdata.sql
@@ -1,4 +1,4 @@
-# MySQL-Front Dump 2.2
+ï»¿# MySQL-Front Dump 2.2
 #
 # Host: localhost   Database: game
 #--------------------------------------------------------
@@ -8,8 +8,8 @@
 #
 # Dumping data for table 'account'
 #
-INSERT INTO account VALUES("minisack","minisack","minisack@sack.de");
-INSERT INTO account VALUES("sack","sack","sack@sack.de");
+INSERT INTO account VALUES('minisack','minisack','minisack@sack.de');
+INSERT INTO account VALUES('sack','sack','sack@sack.de');
 
 
 #
@@ -26,22 +26,22 @@ INSERT INTO account VALUES("sack","sack","sack@sack.de");
 #
 # Dumping data for table 'characterdata'
 #
-INSERT INTO characterdata VALUES("1","Sir Sackerus","1","sack","0","0","0","0","0","0","0","0","0");
-INSERT INTO characterdata VALUES("2","Mr SugaMaDig","1","minisack","0","0","0","0","0","0","0","0","0");
-INSERT INTO characterdata VALUES("3","Mag ma Dig","1","sack","0","0","0","0","0","0","0","0","0");
+INSERT INTO characterdata VALUES('1','Sir Sackerus','1','sack','0','0','0','0','0','0','0','0','0');
+INSERT INTO characterdata VALUES('2','Mr SugaMaDig','1','minisack','0','0','0','0','0','0','0','0','0');
+INSERT INTO characterdata VALUES('3','Mag ma Dig','1','sack','0','0','0','0','0','0','0','0','0');
 #
 # Dumping data for table 'character_status'
 #
-INSERT INTO character_status VALUES("1","A","0","0","1","1","10","0","0","0","0","0","0","0","0","0");
-INSERT INTO character_status VALUES("2","A","0","0","1","10","1","0","0","0","0","0","0","0","0","0");
-INSERT INTO character_status VALUES("3","A","0","0","1","1","10","0","0","0","0","0","0","0","0","0");
+INSERT INTO character_status VALUES('1','A','0','0','1','1','10','0','0','0','0','0','0','0','0','0');
+INSERT INTO character_status VALUES('2','A','0','0','1','10','1','0','0','0','0','0','0','0','0','0');
+INSERT INTO character_status VALUES('3','A','0','0','1','1','10','0','0','0','0','0','0','0','0','0');
 
 #
 # Dumping data for table 'character_visualappearance'
 #
-INSERT INTO character_visualappearance VALUES("1","1","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0");
-INSERT INTO character_visualappearance VALUES("2","2","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0");
-INSERT INTO character_visualappearance VALUES("3","3","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0");
+INSERT INTO character_visualappearance VALUES('1','1','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0');
+INSERT INTO character_visualappearance VALUES('2','2','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0');
+INSERT INTO character_visualappearance VALUES('3','3','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0');
 
 #
 # Dumping data for table 'character_worldobject_mapping'
@@ -57,17 +57,17 @@ INSERT INTO character_visualappearance VALUES("3","3","0","0","0","0","0","0","0
 #
 # Dumping data for table 'item_type'
 #
-INSERT INTO item_type values("1","sword","0","0");
+INSERT INTO item_type values('1','sword','0','0');
 
 
 #
 # Dumping data for table 'item'
 #
-INSERT INTO item values("1","1","Runesword","100","3","1","My Runesword is the best","/models/weapons/rune_sword.3ds","/icons/weapons/rune_sword.png");
+INSERT INTO item values('1','1','Runesword','100','3','1','My Runesword is the best','/models/weapons/rune_sword.3ds','/icons/weapons/rune_sword.png');
 #
 # Dumping data for table 'map'
 #
-INSERT INTO map VALUES("1","Testkarte","");
+INSERT INTO map VALUES('1','Testkarte','');
 
 
 #
@@ -93,7 +93,7 @@ INSERT INTO map VALUES("1","Testkarte","");
 #
 # Dumping data for table 'race'
 #
-INSERT INTO race VALUES("1","human","0","0","0","0","0","0","0","0","Ganz toll, zerstren alles was ihnen in den Weg kommt.");
+INSERT INTO race VALUES('1','human','0','0','0','0','0','0','0','0','Ganz toll, zerstren alles was ihnen in den Weg kommt.');
 
 
 #
@@ -134,13 +134,13 @@ INSERT INTO race VALUES("1","human","0","0","0","0","0","0","0","0","Ganz toll, 
 #
 # Dumping data for table 'language_text_mapping'
 #
-INSERT INTO language_text_mapping VALUES("1","race.1","1","Mensch");
-INSERT INTO language_text_mapping VALUES("2","race.1","2","Human");
+INSERT INTO language_text_mapping VALUES('1','race.1','1','Mensch');
+INSERT INTO language_text_mapping VALUES('2','race.1','2','Human');
 insert into language_text_mapping (id,text_key,languages_id,text) values (3,'yes',1,'ja');
 insert into language_text_mapping (id,text_key,languages_id,text) values (4,'yes',2,'yes');
 insert into language_text_mapping (id,text_key,languages_id,text) values (5,'no',1,'nein');
 insert into language_text_mapping (id,text_key,languages_id,text) values (6,'no',2,'no');
-insert into language_text_mapping (id,text_key,languages_id,text) values (7,'confirm_delete',1,'Wirklich löschen ?');
+insert into language_text_mapping (id,text_key,languages_id,text) values (7,'confirm_delete',1,'Wirklich lschen ?');
 insert into language_text_mapping (id,text_key,languages_id,text) values (8,'confirm_delete',2,'Really delete ?');
 insert into language_text_mapping (id,text_key,languages_id,text) values (9,'menu.button.login',1,'Anmelden');
 insert into language_text_mapping (id,text_key,languages_id,text) values (10,'menu.button.login',2,'Login');
@@ -168,18 +168,18 @@ insert into language_text_mapping (id,text_key,languages_id,text) values (31,'ch
 insert into language_text_mapping (id,text_key,languages_id,text) values (32,'character.selection.button.enterworld',2,'Enter World');
 insert into language_text_mapping (id,text_key,languages_id,text) values (33,'character.selection.button.createcharacter',1,'Erstellen');
 insert into language_text_mapping (id,text_key,languages_id,text) values (34,'character.selection.button.createcharacter',2,'Create');
-insert into language_text_mapping (id,text_key,languages_id,text) values (35,'character.selection.button.deletecharacter',1,'Löschen');
+insert into language_text_mapping (id,text_key,languages_id,text) values (35,'character.selection.button.deletecharacter',1,'Lschen');
 insert into language_text_mapping (id,text_key,languages_id,text) values (36,'character.selection.button.deletecharacter',2,'Delete');
 insert into language_text_mapping (id,text_key,languages_id,text) values (37,'character.selection.label.menuheader',1,'Charakter Menu');
 insert into language_text_mapping (id,text_key,languages_id,text) values (38,'character.selection.label.menuheader',2,'Character Menu');
 insert into language_text_mapping (id,text_key,languages_id,text) values (39,'loadingscreen.label',1,'laden');
 insert into language_text_mapping (id,text_key,languages_id,text) values (40,'loadingscreen.label',2,'loading');
-insert into language_text_mapping (id,text_key,languages_id,text) values (41,'generic.button.back',1,'Zurück');
+insert into language_text_mapping (id,text_key,languages_id,text) values (41,'generic.button.back',1,'Zurck');
 insert into language_text_mapping (id,text_key,languages_id,text) values (42,'generic.button.back',2,'Back');
 insert into language_text_mapping (id,text_key,languages_id,text) values (43,'character.creation.button.create',1,'Erstellen');
 insert into language_text_mapping (id,text_key,languages_id,text) values (44,'character.creation.button.create',2,'Create');
 #
 # Dumping data for table 'languages'
 #
-INSERT INTO languages VALUES("1","de","Deutsch");
-INSERT INTO languages VALUES("2","en","Englisch");
+INSERT INTO languages VALUES('1','de','Deutsch');
+INSERT INTO languages VALUES('2','en','Englisch');
