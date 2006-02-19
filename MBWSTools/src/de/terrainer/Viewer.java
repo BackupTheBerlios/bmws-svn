b36a3f9a1e3c1b6081efd5b20a9b9a0568dbadda
crypter <crypter@5ae0fa9c-bcfc-0310-8243-f99ef3d6a034>
2006-02-19 22:01:34 +0000

    Reformatted.

--- MBWSTools/src/de/terrainer/Viewer.java
+++ MBWSTools/src/de/terrainer/Viewer.java
@@ -5,34 +5,27 @@ import java.io.IOException;
 import org.xml.sax.SAXException;
 
 import com.jme.app.SimpleGame;
-import com.jme.bounding.BoundingBox;
-import com.jme.image.Texture;
 import com.jme.light.DirectionalLight;
 import com.jme.math.Vector3f;
 import com.jme.renderer.ColorRGBA;
 import com.jme.scene.state.CullState;
 import com.jme.scene.state.FogState;
-import com.jme.scene.state.TextureState;
-import com.jme.util.TextureManager;
-import com.jmex.terrain.TerrainBlock;
 
 import de.mbws.client.worldloader.DynamicWorld;
 
 /**
  */
 public class Viewer extends SimpleGame {
-	private int[] map;
-	private int length;
-	private DynamicWorld terrain; 
+	private DynamicWorld terrain;
 
-	public Viewer(int[] map, int length) {
-		this.map = map;
-		this.length = length;
+	public Viewer() {
+		setDialogBehaviour(SimpleGame.NEVER_SHOW_PROPS_DIALOG);
+		
 	}
 
 	protected void simpleInitGame() {
 		// First a hand made terrain
-		//createTerrainFromMap();
+		// createTerrainFromMap();
 		terrain = new DynamicWorld();
 		rootNode.attachChild(terrain);
 		try {
@@ -46,31 +39,31 @@ public class Viewer extends SimpleGame {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		display.getRenderer().setBackgroundColor(new ColorRGBA(0.5f,0.5f,0.5f,1));
-	    DirectionalLight dr = new DirectionalLight();
-	    dr.setEnabled(true);
-	    dr.setDiffuse(new ColorRGBA(0.3f, 0.3f, 0.3f, 1.0f));
-	    dr.setAmbient(new ColorRGBA(1.0f, 1.0f, 1.0f, 1.0f));
-	    dr.setDirection(new Vector3f(0.5f, -0.5f, 0));
-	    lightState.attach(dr);
 
-	    CullState cs = display.getRenderer().createCullState();
-	    cs.setCullMode(CullState.CS_BACK);
-	    cs.setEnabled(true);
-	    rootNode.setRenderState(cs);
+		display.getRenderer().setBackgroundColor(new ColorRGBA(0.5f, 0.5f, 0.5f, 1));
+		DirectionalLight dr = new DirectionalLight();
+		dr.setEnabled(true);
+		dr.setDiffuse(new ColorRGBA(0.3f, 0.3f, 0.3f, 1.0f));
+		dr.setAmbient(new ColorRGBA(1.0f, 1.0f, 1.0f, 1.0f));
+		dr.setDirection(new Vector3f(0.5f, -0.5f, 0));
+		lightState.attach(dr);
 
-	    FogState fs = display.getRenderer().createFogState();
-	    fs.setDensity(0.5f);
-	    fs.setEnabled(true);
-	    fs.setColor(new ColorRGBA(0.5f, 0.5f, 0.5f, 0.5f));
-	    fs.setEnd(1000);
-	    fs.setStart(500);
-	    fs.setDensityFunction(FogState.DF_LINEAR);
-	    fs.setApplyFunction(FogState.AF_PER_VERTEX);
-	    rootNode.setRenderState(fs);
+		CullState cs = display.getRenderer().createCullState();
+		cs.setCullMode(CullState.CS_BACK);
+		cs.setEnabled(true);
+		rootNode.setRenderState(cs);
 
-	    rootNode.updateWorldData(0);
+		FogState fs = display.getRenderer().createFogState();
+		fs.setDensity(0.5f);
+		fs.setEnabled(true);
+		fs.setColor(new ColorRGBA(0.5f, 0.5f, 0.5f, 0.5f));
+		fs.setEnd(1000);
+		fs.setStart(500);
+		fs.setDensityFunction(FogState.DF_LINEAR);
+		fs.setApplyFunction(FogState.AF_PER_VERTEX);
+		rootNode.setRenderState(fs);
+
+		rootNode.updateWorldData(0);
 	}
 
 	@Override
@@ -85,7 +78,6 @@ public class Viewer extends SimpleGame {
 	}
 
 	public void showViewer() {
-		setDialogBehaviour(SimpleGame.NEVER_SHOW_PROPS_DIALOG);
 		start();
 	}
 
@@ -96,62 +88,4 @@ public class Viewer extends SimpleGame {
 		}
 	}
 
-	private void createTerrainFromMap() {
-		try {
-		// The map for our terrain. Each value is a height on the terrain
-		// Create a terrain block. Our integer height values will
-		// scale on the map 2x larger x,
-		// and 2x larger z. Our map's origin will be the regular
-		// origin, and it won't create an
-		// AreaClodMesh from it.
-		Vector3f terrainScale = new Vector3f(5, 0.1f, 5);
-		TerrainBlock tb = new TerrainBlock("block", length, terrainScale, map,
-				new Vector3f(0, 0, 0), false);
-		// Add the texture
-		// This will be the texture for the terrain.
-		TextureState ts = display.getRenderer().createTextureState();
-		ts.setTexture(TextureManager.loadTexture("..\\MBWSClient\\data\\images\\grassb.png", Texture.MM_LINEAR,
-				Texture.FM_LINEAR));
-		tb.setRenderState(ts);
-
-		// Give the terrain a bounding box.
-		tb.setModelBound(new BoundingBox());
-		tb.updateModelBound();
-		// Attach the terrain TriMesh to our rootNode
-		rootNode.attachChild(tb);
-
-		
-		TerrainBlock tb2 = new TerrainBlock("block", length, terrainScale, map,
-				new Vector3f(0, 0, 5*128), false);
-		// Add the texture
-		// This will be the texture for the terrain.
-		TextureState ts2 = display.getRenderer().createTextureState();
-		ts2.setTexture(TextureManager.loadTexture("..\\MBWSClient\\data\\images\\clouds.png", Texture.MM_LINEAR,
-				Texture.FM_LINEAR));
-		tb2.setRenderState(ts2);
-
-		// Give the terrain a bounding box.
-		tb2.setModelBound(new BoundingBox());
-		tb2.updateModelBound();
-		// Attach the terrain TriMesh to our rootNode
-		rootNode.attachChild(tb2);
-
-		/*
-		FogState fs = display.getRenderer().createFogState();
-		fs.setDensity(0.5f);
-		fs.setColor(new ColorRGBA(0.5f,0.5f,0.5f,0.5f));
-		fs.setEnd(60);
-		fs.setStart(40);
-		fs.setDensityFunction(FogState.DF_LINEAR);
-		fs.setApplyFunction(FogState.AF_PER_VERTEX);
-		rootNode.setRenderState(fs);
-		*/
-		}
-		catch (Exception exc) {
-			exc.printStackTrace();
-		}
-	}
-
-	
-	
-	}
+}
