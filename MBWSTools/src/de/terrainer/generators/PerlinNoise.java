7b50e4e24600c25e36741c7d580e4ba2ee0b06de
crypter <crypter@5ae0fa9c-bcfc-0310-8243-f99ef3d6a034>
2006-03-02 21:35:59 +0000

    New generator.

---
+++ MBWSTools/src/de/terrainer/generators/PerlinNoise.java
@@ -0,0 +1,36 @@
+package de.terrainer.generators;
+
+import de.terrainer.AbstractGenerator;
+import de.terrainer.MetaInfo;
+import de.terrainer.gui.HeightMapComponent;
+
+public class PerlinNoise extends AbstractGenerator {
+
+	public PerlinNoise(HeightMapComponent hmc) {
+		super(hmc);
+	}
+
+	@Override
+	public void generate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setMask(int[][] mask) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public MetaInfo[] getMetaInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
