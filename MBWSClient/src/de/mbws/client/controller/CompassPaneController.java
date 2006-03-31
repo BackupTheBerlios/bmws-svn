9fcba729c9a6df5f1dae6ae0efe5f579222242db
moxi_m <moxi_m@5ae0fa9c-bcfc-0310-8243-f99ef3d6a034>
2006-03-31 16:51:57 +0000

    Written.

---
+++ MBWSClient/src/de/mbws/client/controller/CompassPaneController.java
@@ -0,0 +1,38 @@
+package de.mbws.client.controller;
+
+import de.mbws.client.gui.ingame.CompassPane;
+import de.mbws.client.gui.ingame.Orientation;
+
+/**
+ * @author mafw
+ * 
+ */
+public class CompassPaneController {
+    protected CompassPane pane;
+    protected Orientation orientation;
+
+    public CompassPaneController(CompassPane pane) {
+        this.pane = pane;
+        orientation = pane.getOrientation();
+    }
+
+    protected void turnLeft() {
+        orientation.turnLeft();
+        pane.updateDisplay();
+    }
+
+    protected void turnRight() {
+        orientation.turnRight();
+        pane.updateDisplay();
+    }
+
+    public void turnLeft(double degree) {
+        orientation.turnLeft(degree);
+        pane.updateDisplay();
+    }
+
+    public void turnRight(double degree) {
+        orientation.turnRight(degree);
+        pane.updateDisplay();
+    }
+}
