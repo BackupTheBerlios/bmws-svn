76338f95f110cf36d92b743d442b39b77820760f
moxi_m <moxi_m@5ae0fa9c-bcfc-0310-8243-f99ef3d6a034>
2006-03-31 16:53:53 +0000

    Fixed bug, introduced clipping region.

--- MBWSClient/src/de/mbws/client/gui/ingame/GraphixPane.java
+++ MBWSClient/src/de/mbws/client/gui/ingame/GraphixPane.java
@@ -1,52 +1,70 @@
 package de.mbws.client.gui.ingame;
 
-import java.awt.*;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.TexturePaint;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.RectangularShape;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JComponent;
 
 /**
- * A GraphixPane is a Swing component that only shows a portion of an image and allows panning across.
- * Size of the portion and the image are supplied when construction an instance of this type. No
- * restrictions apply to the scrolling so it is done in wrap around mode.
+ * A GraphixPane is a Swing component that only shows a portion of an image and
+ * allows panning across. Size of the portion and the image are supplied when
+ * construction an instance of this type. No restrictions apply to the scrolling
+ * so it is done in wrap around mode.
+ * 
  * @author mafw
- *
+ * 
  */
 public class GraphixPane extends JComponent {
     private static final long serialVersionUID = 1L;
+
     private BufferedImage terrainImage;
+    private int imageWidth;
+    private int imageHeight;
+
     private Dimension size;
     private int offsetX;
     private int offsetY;
+    
+    private RectangularShape clipperShape;
 
     public GraphixPane(Dimension size, BufferedImage terrainImage) {
         this.terrainImage = terrainImage;
         this.size = size;
+        clipperShape = new Rectangle2D.Double();
+        imageWidth = terrainImage.getWidth();
+        imageHeight = terrainImage.getHeight();
+    }
+
+    public void setClipper(RectangularShape shape) {
+        clipperShape = shape;
     }
 
     public Dimension getMinimumSize() {
         return size;
     }
+
     public Dimension getMaximumSize() {
         return size;
     }
+
     public Dimension getPreferredSize() {
         return size;
     }
 
     public void paintComponent(Graphics gfx) {
         super.paintComponent(gfx);
-
-        // Ellipse2D ellipse = new Ellipse2D.Double();
-        // int width = terrainImage.getWidth();
-        // int height = terrainImage.getHeight();
-        // ellipse.setFrame(width >> 2, height >> 2, width >> 4, height >> 4);
-        // gfx.setClip(ellipse);
-
-        Graphics2D g2 = (Graphics2D) gfx;
-        g2.setPaint(new TexturePaint(terrainImage, new Rectangle(offsetX,
-                offsetY, terrainImage.getWidth(), terrainImage.getHeight())));
+        Graphics2D g2 = (Graphics2D) gfx.create();
+        clipperShape.setFrame(0, 0, size.width, size.height);
+        g2.setClip(clipperShape);
+        g2.setPaint(new TexturePaint(terrainImage, new Rectangle(offsetX, offsetY, imageWidth, imageHeight)));
         g2.fillRect(0, 0, size.width, size.height);
+        g2.dispose();
     }
 
     public void panDown() {
@@ -66,10 +84,15 @@ public class GraphixPane extends JComponent {
     }
 
     public void updateDisplay() {
-        repaint();
+        // repaint();
+        getParent().repaint();
     }
-    
+
     public Dimension getImageDimensions() {
         return new Dimension(terrainImage.getWidth(), terrainImage.getHeight());
+    }
+
+    public BufferedImage getImage() {
+        return terrainImage;
     }
 }
